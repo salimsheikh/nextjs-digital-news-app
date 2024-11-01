03e8b07dc975face3e093fcaee4403a40e606197
salimsheikh <salimsheikh4u2000@gmail.com>
2024-11-01 09:14:57 +0530

    Search form completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,19 @@ import React, {useState} from 'react'
 import './header.css';
 import Nav from './Nav';
 import SocialIcons from './SocialIcons';
+import SearchForm from './SearchForm';
+
 
 export default function Header() {
+
+  const [open, setOpen] = useState(false);
+
+  const handleFormOpen = (e: Event | any) => {
+
+    e.preventDefault();
+    setOpen(!open);
+
+  }
   return (
     <header id="header" className='header d-flex align-items-center fixed-top'>
         <div className="container-fluid container-xl d-flex align-item-center justify-content-between">
@@ -17,6 +28,10 @@ export default function Header() {
             <Nav />
             <div className="position-relative">
               <SocialIcons />
+              <a href="#" className="mx-2 js-search-open" onClick={handleFormOpen}>
+                <span className="bi-search"></span>
+              </a>
+              <SearchForm active={open} formOpen={handleFormOpen} />
             </div>
         </div>
     </header>
