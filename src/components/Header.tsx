1eb7f587b6a116eae1e5e3254f10b3de4d5ab60a
salimsheikh <salimsheikh4u2000@gmail.com>
2024-11-01 09:27:58 +0530

    Menu Responisve Done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 import './header.css';
 import Nav from './Nav';
@@ -11,29 +11,40 @@ import SearchForm from './SearchForm';
 export default function Header() {
 
   const [open, setOpen] = useState(false);
+  const [on, setOn] = useState(false);
 
   const handleFormOpen = (e: Event | any) => {
-
     e.preventDefault();
     setOpen(!open);
-
   }
+
+  const handleToggleMenu = () => {
+    setOn(!on);
+    let body:  HTMLElement | any = document.querySelector('body');
+    body.classList.toggle('mobile-nav-active');
+  }
+
   return (
     <header id="header" className='header d-flex align-items-center fixed-top'>
-        <div className="container-fluid container-xl d-flex align-item-center justify-content-between">
-            <a href="/" className='logo d-flex align-item-center'>
-              {/* <img src="" alt="" /> */}
-              <h1>DigitalNews</h1>
-            </a>
-            <Nav />
-            <div className="position-relative">
-              <SocialIcons />
-              <a href="#" className="mx-2 js-search-open" onClick={handleFormOpen}>
-                <span className="bi-search"></span>
-              </a>
-              <SearchForm active={open} formOpen={handleFormOpen} />
-            </div>
+      <div className="container-fluid container-xl d-flex align-item-center justify-content-between">
+        <a href="/" className='logo d-flex align-item-center'>
+          {/* <img src="" alt="" /> */}
+          <h1>DigitalNews</h1>
+        </a>
+        <Nav />
+        <div className="position-relative">
+          <SocialIcons />
+          <a href="#" className="mx-2 js-search-open" onClick={handleFormOpen}>
+            <span className="bi-search"></span>
+          </a>
+          {
+            on ? (
+              <i className="bi bi-x mobile-nav-toggle" onClick={handleToggleMenu}></i>
+            ) : (<i className="bi bi-list mobile-nav-toggle" onClick={handleToggleMenu}></i>)
+          }
+          <SearchForm active={open} formOpen={handleFormOpen} />
         </div>
+      </div>
     </header>
   )
 }
